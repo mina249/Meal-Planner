f3d4022c2e159c1794c0f1be60af074e45881395
AhmedEhab125 <121432823+AhmedEhab125@users.noreply.github.com>
2023-02-08 19:20:18 +0200

    add category and meals with apis

---
+++ app/src/main/java/com/example/mealplaner/Meal/MealData.java
@@ -0,0 +1,16 @@
+package com.example.mealplaner.Meal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mealplaner.R;
+
+public class MealData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_data);
+    }
+}
